127fe2fc7de2565ae96537f5f5354128a79ece0c
itthinx <itthinx@gmail.com>
2014-05-25 20:33:27 +0200

    add-ons content

--- lib/core/affiliates-admin-add-ons.php
+++ lib/core/affiliates-admin-add-ons.php
@@ -24,7 +24,7 @@ if ( !defined( 'ABSPATH' ) ) {
 }
 
 function affiliates_admin_add_ons() {
-	
+
 	echo '<h1>';
 	echo __( 'Affiliates Extensions and Add-Ons', AFFILIATES_PLUGIN_DOMAIN );
 	echo '</h1>';
@@ -33,8 +33,64 @@ function affiliates_admin_add_ons() {
 	echo __( 'Affiliates Pro', AFFILIATES_PLUGIN_DOMAIN );
 	echo '</h2>';
 
+	echo '<ul>';
+
+	echo '<li>';
+	echo __( 'Affiliate attributes for individual commission rates, coupons, ...', AFFILIATES_PLUGIN_DOMAIN );
+	echo '</li>';
+
+	echo '<li>';
+	echo __( 'Fixed, percentage or formula based commissions', AFFILIATES_PLUGIN_DOMAIN );
+	echo '</li>';
+
+	echo '<li>';
+	echo __( 'Extended totals report with additional filters', AFFILIATES_PLUGIN_DOMAIN );
+	echo '</li>';
+
+	echo '<li>';
+	echo __( 'Mass payment file generation', AFFILIATES_PLUGIN_DOMAIN );
+	echo '</li>';
+
+	echo '<li>';
+	echo __( 'Advanced shortcodes including graphs', AFFILIATES_PLUGIN_DOMAIN );
+	echo '</li>';
+
+	echo '<li>';
+	echo __( 'Banner management', AFFILIATES_PLUGIN_DOMAIN );
+	echo '</li>';
+
+	echo '<li>';
+	echo __( 'Notifications including customizable messages', AFFILIATES_PLUGIN_DOMAIN );
+	echo '</li>';
+
+	echo '<li>';
+	echo __( 'Access to integrations with popular e-commerce systems', AFFILIATES_PLUGIN_DOMAIN );
+	echo '</li>';
+
+	echo '</ul>';
+
 	echo '<h2>';
 	echo __( 'Affiliates Enterprise', AFFILIATES_PLUGIN_DOMAIN );
 	echo '</h2>';
+
+	echo '<ul>';
+
+	echo '<li>';
+	echo __( 'Includes all additional features available in Affiliates Pro', AFFILIATES_PLUGIN_DOMAIN );
+	echo '</li>';
+
+	echo '<li>';
+	echo __( 'Multi-tier capability with unlimited levels and rates', AFFILIATES_PLUGIN_DOMAIN );
+	echo '</li>';
+
+	echo '</ul>';
+
+	echo '<h2>';
+	echo __( 'Add-Ons', AFFILIATES_PLUGIN_DOMAIN );
+	echo '</h2>';
+
+	echo '<p>';
+	echo __( 'Free and premium extensions are listed on the <a href="http://www.itthinx.com/plugins/overview/">Overview</a> page and in the <a href="http://www.itthinx.com/shop/">Shop</a>.', AFFILIATES_PLUGIN_DOMAIN );
+	echo '</p>';
 }
 
