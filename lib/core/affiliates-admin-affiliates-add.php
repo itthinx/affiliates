96eb22447e63255a76e9f70eddba4c0d8b860548
eggemplo <eggemplo@gmail.com>
2017-02-20 13:09:46 +0100

    sync from itx

--- lib/core/affiliates-admin-affiliates-add.php
+++ lib/core/affiliates-admin-affiliates-add.php
@@ -254,6 +254,7 @@ function affiliates_admin_affiliates_add_submit() {
 		if ( $wpdb->insert( $affiliates_table, $data_, $formats_ ) ) {
 			$affiliate_id = $wpdb->get_var( "SELECT LAST_INSERT_ID()" );
 		}
+
 		if ( !empty( $affiliate_id ) ) {
 			// user association
 			$new_associated_user_login = trim( $_POST['user-field'] );
@@ -268,8 +269,10 @@ function affiliates_admin_affiliates_add_submit() {
 					}
 				}
 			}
+
 			do_action( 'affiliates_added_affiliate', intval( $affiliate_id ) );
 		}
 	}
+
 	return $result;
 } // function affiliates_admin_affiliates_add_submit
