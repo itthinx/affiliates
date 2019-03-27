8569f90431bc4ce8bde0bd5d5f1036eea60ea532
itthinx <itthinx@gmail.com>
2019-03-28 00:24:16 +0100

    version to 4.0.4; updated changelog; several fixes related to validation and hardening output security

--- js/datepickers.js
+++ js/datepickers.js
@@ -1,39 +1,40 @@
 /**
  * datepickers.js
- * 
+ *
  * Copyright (c) 2010, 2011 "kento" Karim Rahimpur www.itthinx.com
- * 
+ *
  * This code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt.
- * 
+ *
  * This code is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * This header and all notices must be kept intact.
- * 
+ *
  * @author Karim Rahimpur
  * @package affiliates
  * @since affiliates 1.0.0
  */
+
 ( function( $ ) {
-$( document ).ready( function() {
-	// Add a datepicker on .datefield text input fields.
-	$( '.datefield[type="text"]' ).not( '.hasDatePicker' ).datepicker(
-		{
-			dateFormat : 'yy-mm-dd',
-			firstDay   : 1
-		}
-	);
-	// Add a datepicker on date fields where the browser does not support it.
-	if ( $( '[type="date"]' ).prop( 'type' ) != 'date' ) {
-		$( '.datefield[type="date"]' ).not( '.hasDatePicker' ).datepicker(
+	$( document ).ready( function() {
+		// Add a datepicker on .datefield text input fields.
+		$( '.datefield[type="text"]' ).not( '.hasDatePicker' ).datepicker(
 			{
 				dateFormat : 'yy-mm-dd',
 				firstDay   : 1
 			}
 		);
-	}
-} );
+		// Add a datepicker on date fields where the browser does not support it.
+		if ( $( '[type="date"]' ).prop( 'type' ) != 'date' ) {
+			$( '.datefield[type="date"]' ).not( '.hasDatePicker' ).datepicker(
+				{
+					dateFormat : 'yy-mm-dd',
+					firstDay   : 1
+				}
+			);
+		}
+	} );
 } )( jQuery );
