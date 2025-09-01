1009304f1783680344ac8c937801b51685ff1a38
itthinx <itthinx@itthinx.com>
2025-09-01 15:32:57 +0200

    fixed Affiliates Dashboard Profile block preview

--- lib/core/dashboard/class-affiliates-dashboard-profile-block.php
+++ lib/core/dashboard/class-affiliates-dashboard-profile-block.php
@@ -75,6 +75,11 @@ class Affiliates_Dashboard_Profile_Block extends Affiliates_Dashboard_Profile {
 		//	array(),
 		//	AFFILIATES_CORE_VERSION
 		// );
+
+		if ( !wp_script_is( 'affiliates-fields' ) ) {
+			wp_register_style( 'affiliates-fields', AFFILIATES_PLUGIN_URL . 'css/affiliates-fields.css', array(), AFFILIATES_CORE_VERSION, 'all' );
+		}
+		wp_enqueue_style( 'affiliates-fields' );
 	}
 
 	/**
